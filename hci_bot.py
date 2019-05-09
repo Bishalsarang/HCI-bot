d6cd8baad1180215974b688d01ca17cae77d5aa5
Bishal Sarangkoti <sarangbishal@gmail.com>
2019-05-09 21:41:25 +0545

    Make modular

--- hci_bot.py
+++ hci_bot.py
@@ -3,6 +3,38 @@ import re
 import requests
 import time
 
+def print_status(rank, xp, level):
+    """
+    Prints your rank, xp and levek
+    :param rank: Your current rank
+    :param xp: Your current xp
+    :param level: Your current level
+    :return: None
+    """
+    print("You're now at rank {}".format(rank))
+    print("Your current xp is {}".format(xp))
+    print("Your current level is {}".format(level))
+
+
+def is_valid_login(login_page):
+    """
+    Returns boolean value whether the supplied credentials are valid or not
+    :param login_page: requests response object of login page
+    :return: Boolean value whether the login is valid or not
+    """
+    return not re.search("Invalid login, please try again", login_page.text)
+
+
+def get_rank(ladder_html, pattern):
+    """
+    Returns rank from ladder_html page
+    :param ladder_html: requests response object for ladder page
+    :param pattern: Regex pattern to get rank
+    :return: Your current rank
+    """
+    return re.search(pattern, ladder_html.text).group(1)
+
+
 def get_xp_level(pattern_level, pattern_xps, home):
     """
     :param pattern_level:  regex pattern to extract level from homepage
@@ -14,82 +46,101 @@ def get_xp_level(pattern_level, pattern_xps, home):
     current_level = re.search(pattern_level, home.text).group(1)
     current_xp = re.search(pattern_xps, home.text).group(1)
     current_xp = re.search(r'([\d,]+)', current_xp).group(1)
-    return  current_xp, current_level
+    return current_xp, current_level
 
 
-# Your username and password for ELF
-username = ""
-password = ""
+def main():
+    # Your username and password for ELF
+    username = ""
+    password = ""
 
-# ELF Login URL
-LOGIN_URL = "http://elf.ku.edu.np/login/index.php"
+    # ELF Login URL
+    LOGIN_URL = "http://elf.ku.edu.np/login/index.php"
 
-# HCI Course URL
-COURSE_URL = "http://elf.ku.edu.np/course/view.php?id=14"
+    # HCI Course URL
+    COURSE_URL = "http://elf.ku.edu.np/course/view.php?id=14"
 
-postfix = "&redirect=1"
+    # Ladder URL for HCI
+    LADDER_URL = "http://elf.ku.edu.np/blocks/xp/index.php/ladder/14"
 
-# Regex patterns
-pattern_xps = r'<div class=\"pts\">(.+)</div>'
-pattern_level = r'<div class=\".*\"\saria-label=\"Level\s#\d+\">(\d+)</div>'
-# Pattern for link with url
-regex_pattern_1 = r'href=\"http://elf\.ku\.edu\.np/mod/url/view\.php\?id=(\d+)\"'
-# Pattern for link with resources
-regex_pattern_2 = r'href=\"http://elf\.ku\.edu\.np/mod/resource/view\.php\?id=(\d+)\"'
+    # Regex patterns
+    pattern_username = r'<span class=\"usertext\"(?: id=\"\w+\")?>([\w\s-]+)<.*>'
+    pattern_rank = r'<tr class=\"highlight-row\".*?\"\><td class=\".*?>(\d+)</td>'
+    pattern_xps = r'<div class=\"pts\">(.+)</div>'
+    pattern_level = r'<div class=\".*\"\saria-label=\"Level\s#\d+\">(\d+)</div>'
+    # Pattern for link with url
+    regex_pattern_1 = r'href=\"http://elf\.ku\.edu\.np/mod/url/view\.php\?id=(\d+)\"'
+    # Pattern for link with resources
+    regex_pattern_2 = r'href=\"http://elf\.ku\.edu\.np/mod/resource/view\.php\?id=(\d+)\"'
 
-# Base URL for url types link
-BASE_URL_1 = "http://elf.ku.edu.np/mod/url/view.php?id="
-# Base URL for resource type links
-BASE_URL_2 = "http://elf.ku.edu.np/mod/resource/view.php?id="
+    # Base URL for url types link
+    BASE_URL_1 = "http://elf.ku.edu.np/mod/url/view.php?id="
+    # Base URL for resource type links
+    BASE_URL_2 = "http://elf.ku.edu.np/mod/resource/view.php?id="
 
-# Create Session object
-session = requests.session()
-# Login Parameters
-data = {'username': username, 'password': password}
-# Create a new session with given credentials
-sess = session.post(LOGIN_URL, data=data)
+    # Create Session object
+    session = requests.session()
+    # Login Parameters
+    data = {'username': username, 'password': password}
+    # Create a new session with given credentials
+    login_page = session.post(LOGIN_URL, data=data)
 
-# Using the session go to HCI course Page
-home = session.get(COURSE_URL)
+    # If the credentials are invalid
+    if not is_valid_login(login_page):
+        print("Wrong Credentials . Try again!!")
+        exit(0)
 
-xp, level = get_xp_level(pattern_level, pattern_xps, home)
-print("Your initial xp is {}".format(xp))
-print("Your initial level is {}".format(level))
+    # Using the session go to HCI course Page
+    home = session.get(COURSE_URL)
+
+    your_name = re.search(pattern_username, home.text).group(1)
+    rank = get_rank(session.get(LADDER_URL), pattern_rank)
+    xp, level = get_xp_level(pattern_level, pattern_xps, home)
+
+    print("Welcome {}".format(your_name.title()))
+    print_status(rank, xp, level)
+
+    # All links of type URL
+    pattern_1_ids = re.findall(regex_pattern_1, home.text)
+    # All links of type resource
+    pattern_2_ids = re.findall(regex_pattern_2, home.text)
+
+    # Maximum level you want to reach
+    maximum_level_to_reach = 5
+    while True:
+        for i, id in enumerate(pattern_2_ids):
+            print("Clicking Link {}".format(BASE_URL_2 + id))
+            session.get(BASE_URL_2 + id)
+            print("Waiting {} secs".format(10))
+            time.sleep(10)
+
+            # Load homepage to get xp and level
+            home = session.get(COURSE_URL)
+            rank = get_rank(session.get(LADDER_URL), pattern_rank)
+            xp, level = get_xp_level(pattern_level, pattern_xps, home)
+            print_status(rank, xp, level)
+            if int(level) >= maximum_level_to_reach:
+                print("Congratulations {}. You have reached level {}".format(your_name.title(), level))
+                exit(0)
+            time.sleep(10)
+
+        for i, id in enumerate(pattern_1_ids):
+            print("Clicking Link {}".format(BASE_URL_1 + id))
+            session.get(BASE_URL_1 + id)
+            print("Waiting {} secs".format(10))
+            time.sleep(10)
+
+            # Load homepage to get xp and level
+            home = session.get(COURSE_URL)
+            rank = get_rank(session.get(LADDER_URL), pattern_rank)
+            xp, level = get_xp_level(pattern_level, pattern_xps, home)
+            print_status(rank, xp, level)
+            if int(level) >= maximum_level_to_reach:
+                print("Congratulations {}. You have reached level {}".format(your_name.title(), level))
+                exit(0)
+            time.sleep(10)
 
 
-pattern_1_ids = re.findall(regex_pattern_1, home.text)
-pattern_2_ids = re.findall(regex_pattern_2, home.text)
-
-
-
-num_of_times = 20
-
-# Simulate link clicking num_of_times
-for j in range(20):
-
-    for i, id in enumerate(pattern_2_ids):
-        print("Clicking Link {}".format(BASE_URL_2 + id))
-        session.get(BASE_URL_2 + id)
-        print("Waiting {} secs".format(10))
-        time.sleep(10)
-
-        # Load homepage to get xp and level
-        home = session.get(COURSE_URL)
-        xp, level = get_xp_level(pattern_level, pattern_xps, home)
-        print("Your current  xp is {}".format(xp))
-        print("Your current level is {}".format(level))
-        time.sleep(10)
-
-    for i, id in enumerate(pattern_1_ids):
-        print("Clicking Link {}".format(BASE_URL_1 + id))
-        session.get(BASE_URL_1 + id)
-        print("Waiting {} secs".format(10))
-        time.sleep(10)
-
-        # Load homepage to get xp and level
-        home = session.get(COURSE_URL)
-        xp, level = get_xp_level(pattern_level, pattern_xps, home)
-        print("Your current  xp is {}".format(xp))
-        print("Your current level is {}".format(level))
-        time.sleep(10)
+if __name__ == "__main__":
+    main()
 
